5e70c6abb3cd9ce268002952b9591624ea442de8
hayden-donnelly <donnellyhd@outlook.com>
2023-04-10 13:57:19 -0400

    Added configurable param override.

--- scripts/train.py
+++ scripts/train.py
@@ -3,7 +3,21 @@ import numpy as np
 from model import GPT
 import spacy
 
+# Configurable parameters.
 use_spacy = True
+num_blocks = 12
+num_attention_heads = 12
+context_size = 512
+attention_dim = 768
+feed_forward_dim = 3072
+activation = 'gelu'
+token_embed_dim = 384
+dropout = 0.1
+epochs = 1
+batch_size = 1
+
+# Overrides parameters from command line.
+exec(open('configurator.py').read())
 
 # Spacy tokenization.
 def spacy_tokenize(text):
@@ -42,6 +56,7 @@ def get_labels(tokens, token_dim):
     return labels
 
 if __name__ == '__main__':
+    print(batch_size)
     print(tf.config.list_physical_devices())
 
     with open('../data/tiny_shakespeare.txt', 'r', encoding='utf8') as f:
@@ -53,23 +68,23 @@ if __name__ == '__main__':
         tokenized_text, vocab_size = character_tokenize(text)
 
     model = GPT(
-        num_blocks = 12,
-        num_attention_heads = 12,
-        context_size = 512,
-        attention_dim = 768,
-        feed_forward_dim = 3072,
-        activation = 'gelu',
-        token_embed_dim = 384,
-        dropout = 0.1,
+        num_blocks = num_blocks,
+        num_attention_heads = num_attention_heads,
+        context_size = context_size,
+        attention_dim = attention_dim,
+        feed_forward_dim = feed_forward_dim,
+        activation = activation,
+        token_embed_dim = token_embed_dim,
+        dropout = dropout,
         vocab_size = vocab_size
     )
 
-    num_contexts = int(len(tokenized_text) / model.context_size)
-    num_tokens = num_contexts * model.context_size
+    num_contexts = int(len(tokenized_text) / context_size)
+    num_tokens = num_contexts * context_size
 
     input_tokens = np.array(
         tokenized_text[:num_tokens]
-    ).reshape(num_contexts, model.context_size)
+    ).reshape(num_contexts, context_size)
     print("Inputs shape:", input_tokens.shape)
 
     one_hot_labels = np.array(
@@ -86,7 +101,7 @@ if __name__ == '__main__':
     model.fit(
         x = input_tokens, 
         y = one_hot_labels, 
-        epochs = 1, 
-        batch_size = 1,
+        epochs = epochs, 
+        batch_size = batch_size,
         verbose = 1
     )
